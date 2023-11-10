4a8fb3798253e2c1257c46aafa82dc223652a068
gunwoong <gwj0421@gmail.com>
2023-11-10 18:31:08 +0900

    test : 입력된 날짜를 토대로 도메인 영역에서 세밀한 검증하여 객체를 리턴하는 테스트 추가
    - 입력된 날짜가 주어진 범위안에 있는지 검증

---
+++ src/test/java/christmas/domain/DateTest.java
@@ -0,0 +1,29 @@
+package christmas.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class DateTest {
+    private static final String ERROR_MESSAGE_PREFIX = "[ERROR]";
+
+    @DisplayName("주어진 범위를 벗어나는 날짜 입력시 IllegalException 발생")
+    @CsvSource(value = {"12,0","12,32","12,50"})
+    @ParameterizedTest
+    void Should_ThrowException_When_DayIsOutOfRange(int month,int day) {
+        assertThatThrownBy(() -> new Date(month,day))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE_PREFIX);
+    }
+
+    @DisplayName("주어진 범위안의 날짜 입력시 Date 객체 정상 생성")
+    @CsvSource(value = {"12,1", "12,15", "12,31"})
+    @ParameterizedTest
+    void Should_CreateDateObject_When_DayIsInRange(int month, int day) {
+        assertThatCode(() -> new Date(month, day)).doesNotThrowAnyException();
+    }
+
+}
