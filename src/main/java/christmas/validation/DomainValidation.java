befd49f5443f53aa014d075cc73bd29b94d1d7f7
gunwoong <gwj0421@gmail.com>
2023-11-11 10:54:15 +0900

    feat : Domain 영역에서 메뉴와 개수 입력시 검증 기능 추가
    - 입력한 메뉴가 존재하는 메뉴인지 검증

--- src/main/java/christmas/validation/DomainValidation.java
+++ src/main/java/christmas/validation/DomainValidation.java
@@ -1,7 +1,10 @@
 package christmas.validation;
 
 import christmas.config.Month2023;
+import christmas.domain.menu.MenuGroup;
 import christmas.view.ErrorMessage;
+
+import java.util.Map;
 
 public class DomainValidation {
     private DomainValidation() {
@@ -10,6 +13,14 @@ public class DomainValidation {
     public static void validateDateRange(int month, int day) {
         if (!Month2023.isDayInRange(month,day)) {
             throw new IllegalArgumentException(ErrorMessage.DATE_ERROR_MESSAGE.getMessage());
+        }
+    }
+
+    public static void validateMenuExistsOrNot(Map<String, Integer> order) {
+        for (String orderMenu : order.keySet()) {
+            if (MenuGroup.hasMenu(orderMenu) == MenuGroup.EMPTY) {
+                throw new IllegalArgumentException(ErrorMessage.MENU_ERROR_MESSAGE.getMessage());
+            }
         }
     }
 }
