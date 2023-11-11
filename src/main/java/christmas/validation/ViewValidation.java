5ab842233a6c71ec7f92668f41a08ecba029bc30
gunwoong <gwj0421@gmail.com>
2023-11-11 10:51:15 +0900

    feat : View 영역에서 메뉴와 개수 입력시 검증 기능 추가
    - 뷰 영역에서만 진행하기에 아래의 3가지 경우만 검증
        - 입력한 메뉴가 다른 입력한 메뉴와 중복되지 않음을 검증
        - 입력한 개수가 양수임을 검증
        - 입력한 메뉴와 개수가 "음식-개수" 형태를 갖추고 있는지 검증

--- src/main/java/christmas/validation/ViewValidation.java
+++ src/main/java/christmas/validation/ViewValidation.java
@@ -2,6 +2,8 @@ package christmas.validation;
 
 import christmas.view.ErrorMessage;
 
+import java.util.Map;
+
 public class ViewValidation {
     private ViewValidation() {
     }
@@ -20,6 +22,24 @@ public class ViewValidation {
     private static void validateNull(String content) {
         if (content == null) {
             throw new NullPointerException(ErrorMessage.NULL_MESSAGE.getMessage());
+        }
+    }
+
+    public static void validateDuplicated(Map<String, Integer> order,String menu) {
+        if (order.containsKey(menu)) {
+            throw new IllegalArgumentException(ErrorMessage.MENU_ERROR_MESSAGE.getMessage());
+        }
+    }
+
+    public static void validateNegativeOrZero(int num) {
+        if (num < 1) {
+            throw new IllegalArgumentException(ErrorMessage.MENU_ERROR_MESSAGE.getMessage());
+        }
+    }
+
+    public static void validateOrderFormat(String orderPart) {
+        if (orderPart.chars().filter(ch -> ch == '-').count() != 1 || orderPart.charAt(0) == '-' || orderPart.charAt(orderPart.length()-1) == '-') {
+            throw new IllegalArgumentException(ErrorMessage.MENU_ERROR_MESSAGE.getMessage());
         }
     }
 }
