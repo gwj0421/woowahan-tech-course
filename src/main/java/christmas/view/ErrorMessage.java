26c5afa71ff48a136bc6b1ce1e11087f77e5b751
gunwoong <gwj0421@gmail.com>
2023-11-11 10:55:24 +0900

    fix : 에러 메세지 수정

--- src/main/java/christmas/view/ErrorMessage.java
+++ src/main/java/christmas/view/ErrorMessage.java
@@ -5,7 +5,7 @@ public enum ErrorMessage {
     NULL_MESSAGE("[ERROR] Null이 입력되었습니다. 다시 입력해 주세요."),
     MISMATCH_NUMBER_FORMAT("[ERROR] 유효하지 않은 숫자입니다. 다시 입력해 주세요."),
     DATE_ERROR_MESSAGE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
-    MENU_ERROR_MESSAGE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    MENU_ERROR_MESSAGE("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
 
     private final String message;
 
