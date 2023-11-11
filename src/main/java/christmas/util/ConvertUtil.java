fae2c3042cb798ec66a6baa037b27361ca41bb75
gunwoong <gwj0421@gmail.com>
2023-11-11 10:55:11 +0900

    feat : 입력한 문자열을 Map형태로 전환하는 기능 추가

--- src/main/java/christmas/util/ConvertUtil.java
+++ src/main/java/christmas/util/ConvertUtil.java
@@ -2,15 +2,45 @@ package christmas.util;
 
 import christmas.view.ErrorMessage;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static christmas.validation.ViewValidation.*;
+
 public class ConvertUtil {
     private ConvertUtil() {
     }
 
-    public static int string2Int(String content) {
+    public static int convertString2Int(String content) {
         try {
             return Integer.parseInt(content);
         } catch (NumberFormatException e) {
             throw new NumberFormatException(ErrorMessage.MISMATCH_NUMBER_FORMAT.getMessage());
         }
+    }
+
+    public static Map<String, Integer> convertString2Map(String contents) {
+        try {
+            Map<String, Integer> order = new HashMap<>();
+            for (String content : contents.split(",")) {
+                splitMenuAndCnt(content, order);
+            }
+            return order;
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException(ErrorMessage.MISMATCH_NUMBER_FORMAT.getMessage());
+        }
+    }
+
+    private static void splitMenuAndCnt(String content, Map<String, Integer> order) {
+        validateOrderFormat(content);
+        String[] menuAndCnt = content.split("-");
+        String menu = menuAndCnt[0];
+        int cnt = Integer.parseInt(menuAndCnt[1]);
+
+        validateEmptyOrSpaceOrNull(menu);
+        validateDuplicated(order, menu);
+        validateNegativeOrZero(cnt);
+
+        order.put(menu, cnt);
     }
 }
