adac1b9a3ab373e251c03a7ff25ebfa547dbc4db
gunwoong <gwj0421@gmail.com>
2023-11-11 10:46:51 +0900

    feat : 입력된 날짜가 올바른지 올바르지 유무를 리턴하는 기능 테스트 추가

---
+++ src/test/java/christmas/config/Month2023Test.java
@@ -0,0 +1,22 @@
+package christmas.config;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class Month2023Test {
+    @DisplayName("주어진 달에 잘못된 날짜경우 False 리턴")
+    @CsvSource(value = {"12,-1","12,0","12,50"})
+    @ParameterizedTest
+    void Should_ReturnTrue_When_DayIsInvalid(int monthOrder, int day) {
+        Assertions.assertThat(Month2023.isDayInRange(monthOrder,day)).isFalse();
+    }
+
+    @DisplayName("주어진 달에 날짜가 올바른 경우 True 리턴")
+    @CsvSource(value = {"12,1","12,15","12,20"})
+    @ParameterizedTest
+    void Should_ReturnTrue_When_DayIsValid(int monthOrder, int day) {
+        Assertions.assertThat(Month2023.isDayInRange(monthOrder,day)).isTrue();
+    }
+}
