82f63017eecbe48fba084f3d9f44da0744519854
gunwoong <gwj0421@gmail.com>
2023-11-11 10:47:46 +0900

    refactor : 보통 null 처리와 빈 문자열 혹은 공백 처리를 같이하는 경우가 많음으로 하나의 메서드로 묶음

--- src/main/java/christmas/validation/ViewValidation.java
+++ src/main/java/christmas/validation/ViewValidation.java
@@ -6,13 +6,18 @@ public class ViewValidation {
     private ViewValidation() {
     }
 
-    public static void validateEmptyOrSpace(String content) {
+    public static void validateEmptyOrSpaceOrNull(String content) {
+        validateNull(content);
+        validateEmptyOrSpace(content);
+    }
+
+    private static void validateEmptyOrSpace(String content) {
         if (content.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.SPACE_OR_EMPTY_MESSAGE.getMessage());
         }
     }
 
-    public static void validateNull(String content) {
+    private static void validateNull(String content) {
         if (content == null) {
             throw new NullPointerException(ErrorMessage.NULL_MESSAGE.getMessage());
         }
