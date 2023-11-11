fa8e2a08cac45d1e8066819260967304f4817852
gunwoong <gwj0421@gmail.com>
2023-11-11 10:55:48 +0900

    feat : 메뉴와 개수 입력하는 기능 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,6 +2,7 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.domain.Date;
+import christmas.domain.OrderedMenu;
 import christmas.util.ConvertUtil;
 import christmas.validation.ViewValidation;
 
@@ -10,7 +11,7 @@ import static christmas.config.AppConstant.TARGET_MONTH;
 public class InputView {
     private static final String GREETING_MESSAGE = "안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다.\n";
     private static final String REQUEST_VISIT_DATE_MESSAGE = "%d월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n";
-
+    private static final String ORDER_MENU_AND_CNT_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     private InputView() {
     }
 
@@ -26,12 +27,27 @@ public class InputView {
     private static Date readDateWhileVerifying() {
         try {
             String inputContent = Console.readLine();
-            ViewValidation.validateNull(inputContent);
-            ViewValidation.validateEmptyOrSpace(inputContent);
-            return new Date(TARGET_MONTH.getNum(), ConvertUtil.string2Int(inputContent));
+            ViewValidation.validateEmptyOrSpaceOrNull(inputContent);
+            return new Date(TARGET_MONTH.getNum(), ConvertUtil.convertString2Int(inputContent));
         } catch (NullPointerException | IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return readDateWhileVerifying();
+        }
+    }
+
+    public static OrderedMenu inputIOrderMenuAndCnt() {
+        System.out.println(ORDER_MENU_AND_CNT_MESSAGE);
+        return readOrderWhileVerifying();
+    }
+
+    private static OrderedMenu readOrderWhileVerifying() {
+        try {
+            String inputContent = Console.readLine();
+            ViewValidation.validateEmptyOrSpaceOrNull(inputContent);
+            return new OrderedMenu(ConvertUtil.convertString2Map(inputContent));
+        } catch (NullPointerException | IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readOrderWhileVerifying();
         }
     }
 }
