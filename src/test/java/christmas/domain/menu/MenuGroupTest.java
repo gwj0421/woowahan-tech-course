b968bc2766ccd1cfc92c472dc80f9c0c44829f81
gunwoong <gwj0421@gmail.com>
2023-11-14 19:26:17 +0900

    test : 메뉴 그룹을 서칭하는 기능 테스트 추가

--- src/test/java/christmas/domain/menu/MenuGroupTest.java
+++ src/test/java/christmas/domain/menu/MenuGroupTest.java
@@ -1,30 +1,23 @@
 package christmas.domain.menu;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class MenuGroupTest {
-    @DisplayName("없는 메뉴 입력 시 EMPTY 그룹을 리턴")
-    @ValueSource(strings = {"", "타파스", "시저샐러드"})
-    @ParameterizedTest
-    void Should_ReturnMenuGroup_When_MenuIsNotExists(String menu) {
-        // when
-        MenuGroup menuGroup = MenuGroup.hasMenu(menu);
+import static org.assertj.core.api.Assertions.assertThat;
 
-        // then
-        Assertions.assertThat(menuGroup).isEqualTo(MenuGroup.APPETIZER);
+class MenuGroupTest {
+    @DisplayName("제공된 메뉴가 존재하지 않을 경우, Menu.EMPTY 리턴")
+    @ValueSource(strings = {"피자", "햄버거", "민트초코"})
+    @ParameterizedTest
+    void Should_ReturnEmptyGroup_When_GivenMenuNameIsNotExists(String menuName) {
+        assertThat(MenuGroup.getMenuGroupByMenu(menuName)).isEqualTo(MenuGroup.EMPTY);
     }
 
-    @DisplayName("메뉴에 있는 음식 입력 시 해당 음식 그룹을 리턴")
-    @ValueSource(strings = {"파인애플피자", "민트초코", "가지무침"})
+    @DisplayName("제공된 메뉴가 존재할 경우, 해당 메뉴가 있는 MenuGroup 리턴")
+    @ValueSource(strings = {"양송이수프","타파스","시저샐러드"})
     @ParameterizedTest
-    void Should_ReturnMenuGroup_When_MenuIsExists(String menu) {
-        // when
-        MenuGroup menuGroup = MenuGroup.hasMenu(menu);
-
-        // then
-        Assertions.assertThat(menuGroup).isEqualTo(MenuGroup.EMPTY);
+    void Should_ReturnNotEmptyGroup_When_GivenMenuNameIsExists(String menuName) {
+        assertThat(MenuGroup.getMenuGroupByMenu(menuName)).isEqualTo(MenuGroup.APPETIZER);
     }
 }
