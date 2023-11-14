f6a4771544e926c9f6098511b6dfa41e5681843b
gunwoong <gwj0421@gmail.com>
2023-11-14 19:32:17 +0900

    test : 이름 변경에 의한 명칭 변경

--- src/test/java/christmas/config/Month2023DetailTest.java
+++ src/test/java/christmas/config/Month2023DetailTest.java
@@ -5,18 +5,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class Month2023Test {
+class Month2023DetailTest {
     @DisplayName("주어진 달에 잘못된 날짜경우 False 리턴")
     @CsvSource(value = {"12,-1","12,0","12,50"})
     @ParameterizedTest
     void Should_ReturnTrue_When_DayIsInvalid(int monthOrder, int day) {
-        Assertions.assertThat(Month2023.isDayInRange(monthOrder,day)).isFalse();
+        Assertions.assertThat(Month2023Detail.isDayInRange(monthOrder,day)).isFalse();
     }
 
     @DisplayName("주어진 달에 날짜가 올바른 경우 True 리턴")
     @CsvSource(value = {"12,1","12,15","12,20"})
     @ParameterizedTest
     void Should_ReturnTrue_When_DayIsValid(int monthOrder, int day) {
-        Assertions.assertThat(Month2023.isDayInRange(monthOrder,day)).isTrue();
+        Assertions.assertThat(Month2023Detail.isDayInRange(monthOrder,day)).isTrue();
     }
 }
