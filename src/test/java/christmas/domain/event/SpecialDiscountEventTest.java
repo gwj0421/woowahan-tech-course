41c522dd9dd4ccdde57bc201612f8e82ec46f980
gunwoong <gwj0421@gmail.com>
2023-11-14 19:53:42 +0900

    test : 특별 할인 기능 테스트 추가

---
+++ src/test/java/christmas/domain/event/SpecialDiscountEventTest.java
@@ -0,0 +1,45 @@
+package christmas.domain.event;
+
+import christmas.domain.Date;
+import christmas.domain.EventCalender;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SpecialDiscountEventTest {
+    @DisplayName("방문한 날짜에 별이 없다면, 혜택 내역 Map 에 추가하지 않음")
+    @Test
+    void Should_Fail_When_DateIsNotStar() {
+        // given
+        Map<EventType, Integer> discountResult = new LinkedHashMap<>();
+        Set<Date> starPos = Set.of(new Date(12, 1), new Date(12, 2));
+        DiscountEvent event = new SpecialDiscountEvent(new EventCalender(12,starPos));
+
+        // when
+        event.apply(new Date(12, 2), discountResult);
+
+        // then
+        assertThat(discountResult).hasSize(1)
+                .containsEntry(EventType.SPECIAL_DISCOUNT_EVENT, 1000);
+    }
+
+    @DisplayName("방문한 날짜에 별이 있다면, 혜택 내역 Map 에 추가함")
+    @Test
+    void Should_PutMap_When_DateIsStar() {
+        // given
+        Map<EventType, Integer> discountResult = new LinkedHashMap<>();
+        Set<Date> starPos = Set.of(new Date(12, 1), new Date(12, 2));
+        DiscountEvent event = new SpecialDiscountEvent(new EventCalender(12,starPos));
+
+        // when
+        event.apply(new Date(12, 3), discountResult);
+
+        // then
+        assertThat(discountResult).isEmpty();
+    }
+}
