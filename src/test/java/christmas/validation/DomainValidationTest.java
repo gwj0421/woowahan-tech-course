e0f83ff27be28e071ed45f2cf8134b60441c4fca
gunwoong <gwj0421@gmail.com>
2023-11-14 19:42:51 +0900

    test : 도메인 영역에서 검증을 위해 기능 테스트 추가(본문 참고)
    - 음료만 있는 경우 검증
    - 주문한 메뉴 총 개수 검증
    - 입력된 달이 맞는지(1~12월에 해당하는 지) 검증
    - 금액이 음수가 될 경우 검증

--- src/test/java/christmas/validation/DomainValidationTest.java
+++ src/test/java/christmas/validation/DomainValidationTest.java
@@ -56,7 +56,103 @@ class DomainValidationTest {
             order.put("타파스", 2);
 
             // when & then
-            assertThatCode(() -> DomainValidation.validateMenuExistsOrNot(order)).doesNotThrowAnyException();
+            assertThatCode(() -> validateMenuExistsOrNot(order)).doesNotThrowAnyException();
         }
     }
+
+    @Nested
+    class Describe_validateOnlyBeverage_Method {
+        @DisplayName("주어진 메뉴가 음료 메뉴만 있으면 예외 발생")
+        @Test
+        void Should_ThrowException_When_GivenOnlyBeverageMenu() {
+            // given
+            HashMap<String, Integer> order = new HashMap<>();
+            order.put("제로콜라", 1);
+            order.put("레드와인", 2);
+
+            // when & then
+            assertThatThrownBy(() -> validateOnlyBeverage(order))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE_PREFIX);
+        }
+
+        @DisplayName("주어진 메뉴가 음료 메뉴만 있지 않는 경우 정상 통과")
+        @Test
+        void Should_Success_When_GivenNotOnlyBeverageMenu() {
+            // given
+            HashMap<String, Integer> order = new HashMap<>();
+            order.put("제로콜라", 1);
+            order.put("아이스크림", 2);
+
+            // when & then
+            assertThatCode(() -> validateOnlyBeverage(order)).doesNotThrowAnyException();
+        }
+    }
+
+    @Nested
+    class Describe_validateMaxOrderCnt_Method {
+        @DisplayName("주어진 메뉴의 총 개수가 제한한 범위를 넘을 경우 예외 발생")
+        @Test
+        void Should_ThrowException_When_TotalMenuCntIsOutOfRange() {
+            // given
+            HashMap<String, Integer> order = new HashMap<>();
+            order.put("제로콜라", MAX_ORDER_CNT);
+            order.put("레드와인", 5);
+
+            // when & then
+            assertThatThrownBy(() -> validateMaxOrderCnt(order))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE_PREFIX);
+        }
+
+        @DisplayName("주어진 메뉴의 총 개수가 제한된 범위 안일 경우 정상 통과")
+        @Test
+        void Should_Success_When_TotalMenuCntIsRange() {
+            // given
+            HashMap<String, Integer> order = new HashMap<>();
+            order.put("제로콜라", MAX_ORDER_CNT);
+
+            // when & then
+            assertThatCode(() -> validateMaxOrderCnt(order)).doesNotThrowAnyException();
+        }
+    }
+
+    @Nested
+    class Describe_validateMonthRange_Method {
+        @DisplayName("1~12월이 아닌 달을 입력할 경우 예외 발생")
+        @ValueSource(ints = {0, 13, 14})
+        @ParameterizedTest
+        void Should_ThrowException_When_GivenInvalidMonth(int monthOrder) {
+            assertThatThrownBy(() -> validateMonthRange(monthOrder))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE_PREFIX);
+        }
+
+        @DisplayName("1~12월사이 달을 입력할 경우 정상 통과")
+        @ValueSource(ints = {1,5,12})
+        @ParameterizedTest
+        void Should_Success_When_GivenValidMonth(int monthOrder) {
+            assertThatCode(() -> validateMonthRange(monthOrder)).doesNotThrowAnyException();
+        }
+    }
+
+    @Nested
+    class Describe_validateMoneyIsNegative_Method {
+        @DisplayName("금액이 음수일 경우 경우 예외 발생")
+        @ValueSource(ints = {-1,-5,-10})
+        @ParameterizedTest
+        void Should_ThrowException_When_GivenNegativeMoney(int money) {
+            assertThatThrownBy(() -> validateMoneyIsNegative(money))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(ERROR_MESSAGE_PREFIX);
+        }
+
+        @DisplayName("금액이 0 이상일 경우 정상 통과")
+        @ValueSource(ints = {0,5,10})
+        @ParameterizedTest
+        void Should_Success_When_GivenNotNegativeMoney(int money) {
+            assertThatCode(() -> validateMoneyIsNegative(money)).doesNotThrowAnyException();
+        }
+    }
+
 }
