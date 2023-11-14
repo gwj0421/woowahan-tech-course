40dbeb739481ab4da4eb2ae8b39e8ffa4cd11c8e
gunwoong <gwj0421@gmail.com>
2023-11-14 19:31:24 +0900

    style : final 처리

--- src/main/java/christmas/validation/DomainValidation.java
+++ src/main/java/christmas/validation/DomainValidation.java
@@ -10,17 +10,38 @@ public class DomainValidation {
     private DomainValidation() {
     }
 
-    public static void validateDateRange(int month, int day) {
-        if (!Month2023.isDayInRange(month,day)) {
+    public static void validateDateRange(final int month, final int day) {
+        if (!Month2023Detail.isDayInRange(month, day)) {
             throw new IllegalArgumentException(ErrorMessage.DATE_ERROR_MESSAGE.getMessage());
         }
     }
 
-    public static void validateMenuExistsOrNot(Map<String, Integer> order) {
+    public static void validateMenuExistsOrNot(final Map<String, Integer> order) {
         for (String orderMenu : order.keySet()) {
-            if (MenuGroup.hasMenu(orderMenu) == MenuGroup.EMPTY) {
+            if (MenuGroup.getMenuGroupByMenu(orderMenu) == MenuGroup.EMPTY) {
                 throw new IllegalArgumentException(ErrorMessage.MENU_ERROR_MESSAGE.getMessage());
             }
+        }
+    }
+
+    public static void validateOnlyBeverage(final Map<String, Integer> order) {
+        for (Map.Entry<String, Integer> orderEntry : order.entrySet()) {
+            if (!MenuGroup.getMenuGroupByMenu(orderEntry.getKey()).equals(MenuGroup.BEVERAGE)) {
+                return;
+            }
+        }
+
+        throw new IllegalArgumentException(ErrorMessage.MENU_ERROR_MESSAGE.getMessage());
+    }
+
+    public static void validateMaxOrderCnt(final Map<String, Integer> order) {
+        int totalCnt = 0;
+        for (Integer cnt : order.values()) {
+            totalCnt += cnt;
+        }
+
+        if (totalCnt < MIN_ORDER_CNT || MAX_ORDER_CNT < totalCnt) {
+            throw new IllegalArgumentException(ErrorMessage.MENU_ERROR_MESSAGE.getMessage());
         }
     }
 }
