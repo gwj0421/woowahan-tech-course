363ca9159d9aaeb154f30c56a8f8cbc402a9022e
gunwoong <gwj0421@gmail.com>
2023-11-14 20:00:26 +0900

    feat : 총 혜택 내역 및 증정품 목록, 실제 혜택 금액을 객체화

---
+++ src/main/java/christmas/domain/PreviewResult.java
@@ -0,0 +1,38 @@
+package christmas.domain;
+
+import christmas.domain.event.EventType;
+import christmas.domain.menu.Menu;
+
+import java.util.Map;
+
+public class PreviewResult {
+    private final Map<EventType, Integer> benefitResult;
+    private final Map<Menu,Integer> giveawayResult;
+    private final int actualPayAmount;
+
+    public PreviewResult(final Map<EventType, Integer> benefitResult, final Map<Menu, Integer> giveawayResult, final int actualPayAmount) {
+        this.benefitResult = benefitResult;
+        this.giveawayResult = giveawayResult;
+        this.actualPayAmount = actualPayAmount;
+    }
+
+    public Map<EventType, Integer> getBenefitResult() {
+        return benefitResult;
+    }
+
+    public Map<Menu, Integer> getGiveawayResult() {
+        return giveawayResult;
+    }
+
+    public int getActualPayAmount() {
+        return actualPayAmount;
+    }
+
+    public final int getTotalBenefitAmount() {
+        int totalBenefitAmount = 0;
+        for (Integer benefitAmount : getBenefitResult().values()) {
+            totalBenefitAmount += benefitAmount;
+        }
+        return totalBenefitAmount;
+    }
+}
