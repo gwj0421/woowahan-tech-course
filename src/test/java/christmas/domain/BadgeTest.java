e9c423077f976909c829f1ac5d85ff1c47c4b35a
gunwoong <gwj0421@gmail.com>
2023-11-14 20:02:54 +0900

    test : 혜택에 따른 뱃지 제공 기능 테스트 추가

---
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -0,0 +1,16 @@
+package christmas.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class BadgeTest {
+
+    @DisplayName("총혜택 금액이 뱃지 별 최소 금액을 달성시 해당 뱃지 리턴")
+    @CsvSource(value = {"0,NORMAL","4900,NORMAL","5000,STAR","5100,STAR","10000,TREE","11000,TREE","20000,SANTA","30000,SANTA"})
+    @ParameterizedTest
+    void Should_ReturnBadge_When_GivenTotalBenefitAmount(int totalBenefitAmount, Badge actualBadge) {
+        Assertions.assertThat(Badge.giveBadge(totalBenefitAmount)).isEqualTo(actualBadge);
+    }
+}
