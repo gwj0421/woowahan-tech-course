4ca41041f0c5476be91effa0301411ef8da582b6
gunwoong <gwj0421@gmail.com>
2023-11-14 19:39:07 +0900

    feat : 12월 뿐만 아니라 다른 달에도 사용할 수 있도록 일뿐만 아니라 달, 요일 추가
    해당 방법을 통해 이벤트 적용시 요일, 일수에 따른 조건을 따질수 있게 되었음

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -1,16 +1,39 @@
 package christmas.domain;
 
+import christmas.domain.week.Week;
 import christmas.validation.DomainValidation;
 
 public class Date {
+    private final int month;
     private final int day;
+    private Week week;
 
-    public Date(int month, int day) {
+    public Date(final int month, final int day) {
         validate(month, day);
+        this.month = month;
         this.day = day;
     }
 
-    private static void validate(int month, int day) {
+    public Date(final int month, final int day, final Week week) {
+        validate(month, day);
+        this.month = month;
+        this.day = day;
+        this.week = week;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public Week getWeek() {
+        return week;
+    }
+
+    private static void validate(final int month, final int day) {
         DomainValidation.validateDateRange(month, day);
     }
 }
