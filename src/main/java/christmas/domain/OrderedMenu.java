fb181844d0b1ed7deeebca633cd776f431a0fe28
gunwoong <gwj0421@gmail.com>
2023-11-14 19:35:55 +0900

    feat : 입력된 메뉴와 개수를 통해 총 가격 리턴하는 기능 추가

--- src/main/java/christmas/domain/OrderedMenu.java
+++ src/main/java/christmas/domain/OrderedMenu.java
@@ -5,15 +5,29 @@ import christmas.validation.DomainValidation;
 
 import java.util.Map;
 
-public class OrderedMenu {
-    private final Map<String, Integer> order;
+import static christmas.util.ConvertUtil.convertKeyString2Menu;
 
-    public OrderedMenu(Map<String, Integer> order) {
+public class OrderedMenu {
+    private final Map<Menu, Integer> order;
+
+    public OrderedMenu(final Map<String, Integer> order) {
         validate(order);
-        this.order = order;
+        this.order = convertKeyString2Menu(order);
     }
 
-    private static void validate(Map<String, Integer> order) {
+    public Map<Menu, Integer> getOrder() {
+        return order;
+    }
+
+    public final int getTotalPrice() {
+        int total = 0;
+        for (Map.Entry<Menu, Integer> orderEntry : getOrder().entrySet()) {
+            total += orderEntry.getKey().getPrice() * orderEntry.getValue();
+        }
+        return total;
+    }
+
+    private static void validate(final Map<String, Integer> order) {
         DomainValidation.validateMenuExistsOrNot(order);
         DomainValidation.validateOnlyBeverage(order);
         DomainValidation.validateMaxOrderCnt(order);
