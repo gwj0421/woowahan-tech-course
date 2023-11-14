a1f432fe60641777b6b894e02905f86a4e358cf7
gunwoong <gwj0421@gmail.com>
2023-11-14 19:58:20 +0900

    feat : 샴페인 증정 이벤트 추가
    - 120000원 이상 구매시 샴페인 증정

---
+++ src/main/java/christmas/domain/event/GiveawayByTotalPurchaseAmountEvent.java
@@ -0,0 +1,33 @@
+package christmas.domain.event;
+
+import christmas.domain.OrderedMenu;
+import christmas.domain.menu.Menu;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class GiveawayByTotalPurchaseAmountEvent extends GiveawayEvent<Integer> {
+    public GiveawayByTotalPurchaseAmountEvent() {
+        super(Map.of(120000, Map.of(Menu.CHAMPAGNE, 1)));
+    }
+
+    @Override
+    public Map<Menu, Integer> apply(final OrderedMenu orderedMenu, final Map<EventType, Integer> benefitDetail) {
+        int totalPurchaseAmount = orderedMenu.getTotalPrice();
+        for (Map.Entry<Integer, Map<Menu, Integer>> informationEntry : this.getGiveawayInformation().entrySet()) {
+            if (informationEntry.getKey() <= totalPurchaseAmount) {
+                return getTotalGiveawayAmount(informationEntry.getValue(), benefitDetail);
+            }
+        }
+        return Collections.emptyMap();
+    }
+
+    private Map<Menu, Integer> getTotalGiveawayAmount(final Map<Menu, Integer> receivableGiveaway, final Map<EventType, Integer> benefitDetail) {
+        int totalGiveawayAmount = 0;
+        for (Map.Entry<Menu, Integer> giveawayAndCnt : receivableGiveaway.entrySet()) {
+            totalGiveawayAmount += giveawayAndCnt.getKey().getPrice() * giveawayAndCnt.getValue();
+        }
+        benefitDetail.put(EventType.GIVEAWAY_EVENT, totalGiveawayAmount);
+        return receivableGiveaway;
+    }
+}
