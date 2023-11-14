4a2f6527609a410301e1eccc97caf7f30390399b
gunwoong <gwj0421@gmail.com>
2023-11-14 20:04:41 +0900

    feat : 날짜와 주문 내역을 토대로 이벤트 적용 유무 테스트 및 이벤트가 잘 적용되는지 테스트 추가

---
+++ src/test/java/christmas/domain/event/PreviewDiscountMachineTest.java
@@ -0,0 +1,58 @@
+package christmas.domain.event;
+
+import christmas.domain.Date;
+import christmas.domain.EventCalender;
+import christmas.domain.OrderedMenu;
+import christmas.domain.PreviewResult;
+import christmas.domain.menu.Menu;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PreviewDiscountMachineTest {
+    @DisplayName("총 주문 금액이 이벤트 최소 적용 금액을 채우지 못한 경우 이벤트 적용 생략")
+    @Test
+    void Should_NotApplyEvent_When_OrderAmountIsUnderMinimum() {
+        // given
+        Map<String, Integer> order = new HashMap<>();
+        order.put("타파스", 1);
+        order.put("제로콜라", 1);
+
+        // when
+        PreviewDiscountMachine prev = new PreviewDiscountMachine(new EventCalender(12, Set.of()), new OrderedMenu(order), 12);
+        PreviewResult prevResult = prev.getPreviewResult();
+
+        // then
+        assertThat(prevResult.getBenefitResult()).isEmpty();
+        assertThat(prevResult.getGiveawayResult()).isEmpty();
+
+    }
+
+    @DisplayName("총 주문 금액이 이벤트 최소 금액 이상일 경우 적용할 수 있는 이벤트 적용")
+    @Test
+    void Should_ApplyEvent_When_OrderAmountIsOverMinimum() {
+        // given
+        Map<String, Integer> order = new HashMap<>();
+        order.put("티본스테이크", 1);
+        order.put("바비큐립", 1);
+        order.put("초코케이크", 2);
+        order.put("제로콜라", 1);
+
+        // when
+        PreviewDiscountMachine prev = new PreviewDiscountMachine(new EventCalender(12, Set.of(new Date(12, 3))), new OrderedMenu(order), 3);
+        PreviewResult prevResult = prev.getPreviewResult();
+
+        // then
+        assertThat(prevResult.getBenefitResult()).hasSize(4).
+                containsEntry(EventType.CHRISTMAS_D_DAY_DISCOUNT_EVENT, 1200).
+                containsEntry(EventType.WEEKDAY_DISCOUNT_EVENT, 4046).
+                containsEntry(EventType.SPECIAL_DISCOUNT_EVENT, 1000).
+                containsEntry(EventType.GIVEAWAY_EVENT, 25000);
+        assertThat(prevResult.getGiveawayResult()).hasSize(1).containsEntry(Menu.CHAMPAGNE, 1);
+    }
+}
