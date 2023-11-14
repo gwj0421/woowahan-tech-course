d6f5a05b56d893a3571a192068b24241426d40a1
gunwoong <gwj0421@gmail.com>
2023-11-14 19:50:45 +0900

    test : 이벤트를 다루기 위해 날짜 입력시 해당 이벤트의 기간인지 유무를 리턴하는 기능 테스트 추가

---
+++ src/test/java/christmas/domain/event/EventTypeTest.java
@@ -0,0 +1,34 @@
+package christmas.domain.event;
+
+import christmas.domain.Date;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class EventTypeTest {
+    @DisplayName("주어진 날짜에 해당 이벤트 기간이 아니면 False 리턴")
+    @Test
+    void Should_ReturnFalse_When_DateIsInvalid() {
+        // given
+        Date date = new Date(12, 26);
+
+        // when
+        boolean result = EventType.CHRISTMAS_D_DAY_DISCOUNT_EVENT.isEventApply(date);
+
+        // then
+        Assertions.assertThat(result).isFalse();
+    }
+
+    @DisplayName("주어진 날짜에 해당 이벤트 기간이면 True 리턴")
+    @Test
+    void Should_ReturnTrue_When_DateIsValid() {
+        // given
+        Date date = new Date(12, 25);
+
+        // when
+        boolean result = EventType.CHRISTMAS_D_DAY_DISCOUNT_EVENT.isEventApply(date);
+
+        // then
+        Assertions.assertThat(result).isTrue();
+    }
+}
