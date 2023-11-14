e5d3b5dbf10544f82be6fbf5e011c11ac733839b
gunwoong <gwj0421@gmail.com>
2023-11-14 19:34:40 +0900

    test : 메뉴와 개수를 입력받을 때 검증 테스트 추가
    - 없는 메뉴 검증
    - 음료 메뉴만 입력했는지 검증
    - 최대 개수를 넘기는지 검증

--- src/test/java/christmas/domain/OrderedMenuTest.java
+++ src/test/java/christmas/domain/OrderedMenuTest.java
@@ -1,10 +1,11 @@
 package christmas.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.*;
 
 class OrderedMenuTest {
     private static final String ERROR_MESSAGE_PREFIX = "[ERROR]";
@@ -14,13 +15,41 @@ class OrderedMenuTest {
     void Should_ThrowException_When_MenuIsNotExists() {
         // given
         HashMap<String, Integer> order = new HashMap<>();
-        order.put("시저샐러드",1);
-        order.put("양송이수프",2);
+        order.put("시저샐러드", 1);
+        order.put("양송이수프", 2);
         order.put("파인애플피자", 3);
 
         // when & then
-        Assertions.assertThatThrownBy(() -> new OrderedMenu(order))
+        assertThatThrownBy(() -> new OrderedMenu(order))
                 .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE_PREFIX);
+    }
+
+    @DisplayName("음료 메뉴만 입력 시 예외 발생")
+    @Test
+    void Should_ThrowException_When_EnterOnlyBeverage() {
+        // given
+        HashMap<String, Integer> order = new HashMap<>();
+        order.put("제로콜라", 1);
+        order.put("레드와인", 2);
+
+        // when & then
+        assertThatThrownBy(() -> new OrderedMenu(order)).
+                isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE_PREFIX);
+    }
+
+    @DisplayName("메뉴 최대 개수를 넘게 입력 시 예외 발생")
+    @Test
+    void Should_ThrowException_When_ExceedMaximumMenuCnt() {
+        // given
+        HashMap<String, Integer> order = new HashMap<>();
+        order.put("시저샐러드", 15);
+        order.put("양송이수프", 15);
+
+        // when & then
+        assertThatThrownBy(() -> new OrderedMenu(order)).
+                isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE_PREFIX);
     }
 
@@ -29,27 +58,26 @@ class OrderedMenuTest {
     void Should_CreateOrderMenuObject_When_MenuIsExists() {
         // given
         HashMap<String, Integer> order = new HashMap<>();
-        order.put("시저샐러드",1);
-        order.put("양송이수프",2);
+        order.put("시저샐러드", 1);
+        order.put("양송이수프", 2);
 
         // when & then
-        Assertions.assertThatCode(() -> new OrderedMenu(order)).doesNotThrowAnyException();
+        assertThatCode(() -> new OrderedMenu(order)).doesNotThrowAnyException();
     }
 
-    @DisplayName("입력된 메뉴들의 총 가격을 계산하여 계산 결과를 리턴")
+    @DisplayName("메뉴와 개수 입력 시 총 금액 리턴")
     @Test
-    void Should_ReturnTotalPrice_When_MenuIsExists() {
+    void Should_ReturnTotalPrice_When_EnterMenuAndCnt() {
         // given
         HashMap<String, Integer> order = new HashMap<>();
-        order.put("시저샐러드",1);
-        order.put("양송이수프",2);
-        int totalActualPrice = 8000 + 6000 * 2;
+        order.put("시저샐러드", 1);
+        order.put("양송이수프", 2);
 
         // when
         OrderedMenu orderedMenu = new OrderedMenu(order);
+        int expectedPrice = orderedMenu.getTotalPrice();
 
         // then
-        Assertions.assertThat(orderedMenu.getTotalPrice()).isEqualTo(totalActualPrice);
+        assertThat(expectedPrice).isEqualTo(20000);
     }
-
 }
