904d4a262e933009ce9c318f619092317b922e81
gunwoong <gwj0421@gmail.com>
2023-11-14 19:27:11 +0900

    test : 입력된 문자열을 통해 메뉴를 찾는 기능 테스트 추가

--- src/test/java/christmas/domain/menu/MenuTest.java
+++ src/test/java/christmas/domain/menu/MenuTest.java
@@ -1,15 +1,26 @@
 package christmas.domain.menu;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MenuTest {
-    @DisplayName("메뉴 입력 시, 가격 리턴")
-    @CsvSource(value = {"양송이수프,6000", "타파스,5500"})
+    @DisplayName("입력된 문자열로 된 메뉴가 없는 메뉴일 경우, IllegalArgumentException 예외 발생")
+    @ValueSource(strings = {"피자", "햄버거", "민트초코"})
     @ParameterizedTest
-    void Should_ReturnPrice_When_EnterMenu(String menuName, int price) {
-        Assertions.assertThat(Menu.getPriceByMenuName(menuName)).isEqualTo(price);
+    void Should_ThrowException_When_GivenMenuNameIsNotExists(String menu) {
+        assertThatThrownBy(() -> Menu.getMenuByMenuName(menu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력된 문자열로 된 메뉴가 존재하는 메뉴일 경우, Menu 리턴")
+    @EnumSource(names = {"MUSHROOM_SOUP", "T_BONE_STEAK", "RED_WINE"})
+    @ParameterizedTest
+    void Should_ReturnMenu_When_GivenMenuNameIsExists(Menu menu) {
+        assertThat(Menu.getMenuByMenuName(menu.getName())).isEqualTo(menu);
     }
 }
