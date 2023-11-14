ffed8a785345c01bd52488ab82b4fab9f3e11fa5
gunwoong <gwj0421@gmail.com>
2023-11-14 19:45:54 +0900

    feat : 개발 과정에서 발생하는 에러를 디버깅을 위해 메세지 추가

--- src/main/java/christmas/view/ErrorMessage.java
+++ src/main/java/christmas/view/ErrorMessage.java
@@ -5,11 +5,11 @@ public enum ErrorMessage {
     NULL_MESSAGE("[ERROR] Null이 입력되었습니다. 다시 입력해 주세요."),
     MISMATCH_NUMBER_FORMAT("[ERROR] 유효하지 않은 숫자입니다. 다시 입력해 주세요."),
     DATE_ERROR_MESSAGE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
-    MENU_ERROR_MESSAGE("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-
+    MENU_ERROR_MESSAGE("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    DEVELOP_ERROR_MESSAGE("[ERROR] 코드 에러. 잘못된 파라미터를 입력받았습니다.");
     private final String message;
 
-    ErrorMessage(String message) {
+    ErrorMessage(final String message) {
         this.message = message;
     }
 
